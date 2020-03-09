4f0f97c36ddc25833f5a6518ec6bc0f75fa3579d
Benoit Blanchon <git@benoitblanchon.fr>
2020-03-09 21:30:25 +0100

    Add background color. Resize windows according to text

--- app/mainwindow.c
+++ app/mainwindow.c
@@ -4,8 +4,30 @@
 #include "stdafx.h"
 
 #define FLASH_TIMER_ID 1
+#define TEXT_COLOR RGB(255, 255, 255)
+#define BACKGROUND_COLOR RGB(0, 121, 216)
+#define PADDING_TOP 20
+#define PADDING_BOTTOM 20
+#define PADDING_RIGHT 30
+#define PADDING_LEFT 30
+#define MAIN_WINDOW_STYLE                                                      \
+  (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX)
 
 static HFONT hFont;
+static HPEN hPen;
+
+static void DrawExplanationText(HDC hdc, RECT *rect, UINT format) {
+  HINSTANCE hInstance = GetModuleHandle(NULL);
+
+  TCHAR szExplanation[128];
+  size_t nExplanation = LoadString(hInstance, IDS_EXPLANATION, szExplanation,
+                                   ARRAYSIZE(szExplanation));
+
+  SetTextColor(hdc, TEXT_COLOR);
+  SetBkMode(hdc, TRANSPARENT);
+  SelectObject(hdc, hFont);
+  DrawText(hdc, szExplanation, nExplanation, rect, format);
+}
 
 static void OnPaint(HWND hwnd) {
   PAINTSTRUCT ps;
@@ -14,19 +36,11 @@ static void OnPaint(HWND hwnd) {
   RECT rect;
   GetClientRect(hwnd, &rect);
 
-  SelectObject(hdc, hFont);
-
-  HINSTANCE hInstance = GetModuleHandle(NULL);
-
-  TCHAR szExplanation[128];
-  size_t nExplanation = LoadString(hInstance, IDS_EXPLANATION, szExplanation,
-                                   ARRAYSIZE(szExplanation));
-
-  rect.top = 20;
-  rect.left = 40;
-  rect.bottom -= 20;
-  rect.right -= 40;
-  DrawText(hdc, szExplanation, nExplanation, &rect, DT_TOP | DT_LEFT);
+  rect.top += PADDING_TOP;
+  rect.left = PADDING_LEFT;
+  rect.bottom -= PADDING_BOTTOM;
+  rect.right -= PADDING_RIGHT;
+  DrawExplanationText(hdc, &rect, DT_TOP | DT_LEFT | DT_NOCLIP);
 
   EndPaint(hwnd, &ps);
 }
@@ -86,14 +100,27 @@ void RegisterMainWindowClass(HINSTANCE hInstance) {
   wcex.hInstance = hInstance;
   wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPICON));
   wcex.hCursor = 0;
-  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.hbrBackground = CreateSolidBrush(BACKGROUND_COLOR);
   wcex.lpszMenuName = 0;
   wcex.lpszClassName = MAIN_WINDOW_CLASS;
   wcex.hIconSm = 0;
   RegisterClassEx(&wcex);
 
-  hFont =
-      CreateFont(30, 0, 0, 0, 300, FALSE, FALSE, FALSE, 0, 0, 0, 0, FF_SWISS, TEXT("Segoe UI Light"));
+  hFont = CreateFont(32, 0, 0, 0, FW_LIGHT, FALSE, FALSE, FALSE,
+                     DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+                     PROOF_QUALITY, FF_SWISS, TEXT("Segoe UI Light"));
+}
+void MeasureSize(SIZE *sz) {
+  RECT rect = {0, 0, 450, 200};
+
+  HDC hdc = GetDC(NULL);
+  DrawExplanationText(hdc, &rect, DT_CALCRECT | DT_TOP | DT_LEFT);
+  ReleaseDC(NULL, hdc);
+
+  AdjustWindowRect(&rect, MAIN_WINDOW_STYLE, FALSE);
+
+  sz->cx = rect.right - rect.left + PADDING_LEFT + PADDING_RIGHT;
+  sz->cy = rect.bottom - rect.top + PADDING_TOP + PADDING_BOTTOM;
 }
 
 HWND CreateMainWindow(HINSTANCE hInstance) {
@@ -102,8 +129,10 @@ HWND CreateMainWindow(HINSTANCE hInstance) {
       LoadString(hInstance, IDS_TITLE, szTitle, ARRAYSIZE(szTitle) - 1);
   szTitle[nTitle] = 0;
 
-  return CreateWindow(MAIN_WINDOW_CLASS, szTitle,
-                      (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU |
-                       WS_MINIMIZEBOX | WS_MAXIMIZEBOX),
-                      CW_USEDEFAULT, 0, 450, 150, NULL, NULL, hInstance, NULL);
+  SIZE sz;
+  MeasureSize(&sz);
+
+  return CreateWindow(MAIN_WINDOW_CLASS, szTitle, MAIN_WINDOW_STYLE,
+                      CW_USEDEFAULT, 0, sz.cx, sz.cy, NULL, NULL, hInstance,
+                      NULL);
 }
