29740fe687b9aadbdb871f607dee470babe9933d
Benoit Blanchon <git@benoitblanchon.fr>
2023-05-12 10:51:21 +0200

    Remove Google Analytics

--- _includes/js/ecommerce.js
+++ _includes/js/ecommerce.js
@@ -1,11 +1,4 @@
 function reportPurchase(transaction) {
-  dataLayer.push({
-    'event': 'generic',
-    'eventCategory': 'ecommerce',
-    'eventAction': 'purchase',
-    'eventLabel': transaction.buyer,
-    'eventValue': parseInt(transaction.total)
-  })
   plausible('Purchase', {
     props: { total: transaction.total, currency: transaction.currency }
   })
